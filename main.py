915c0a7ba56dd62c58372ea97bd515accf99aa25
vogronicspatrik <vogronicspatrik@gmail.com>
2016-09-13 09:57:07 +0200

    4/7

--- main.py
+++ main.py
@@ -1,3 +1,35 @@
+class ContactList(list):
+
+    def search(self, string):
+        solution = []
+        for i in self:
+            if string in i.name:
+                solution.append(i)
+        return solution
+
+    def longest_name(self):
+        s = {i.name: len(i.name) for i in self}
+        mylist = sorted(s, key=s.items(), reverse=True)[0]
+        return mylist
+
 
 class Contact:
-    pass
+    all_contacts = ContactList()
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        Contact.all_contacts.append(self)
+
+    @classmethod
+    def reset_contacts(cls):
+        cls.all_contacts = []
+        return cls.all_contacts
+
+
+class Supplier(Contact):
+    all_orders = {}
+
+    def order(self, string):
+        new = {self.name: string}
+        Supplier.all_orders.update(new)
